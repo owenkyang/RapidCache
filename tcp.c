6ae8b56ebb31a34f6b6e09d30068ac55b08b9f50
owenkyang <owen.yangkeding@gmail.com>
2024-09-14 09:44:42 -0700

    test

--- tcp.c
+++ tcp.c
@@ -1,2 +1,10 @@
-int fd = socket(AF_INET, SOCK_STREAM, 0);
+#include <stdio.h>      // For printf
+#include <stdlib.h>     // For exit
+#include <string.h>     // For memset
+#include <arpa/inet.h>  // For sockaddr_in and inet_addr
+#include <sys/socket.h> // For socket functions
+#include <unistd.h> 
+int main(){
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+}
 
