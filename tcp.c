6f907774120d4b3857a6b29827818197673ef176
owenkyang <owentheyanger@gmail.com>
2024-09-17 11:05:31 -0700

    tcp server

--- tcp.c
+++ tcp.c
@@ -7,5 +7,15 @@
 int main(){
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("bind()");
+    }
 }
 
